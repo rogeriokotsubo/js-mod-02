3c14b46ad01ec80cb0ecfa3349307685700654c7
Rogerio Kotsubo <rogeriokotsubo@gmail.com>
2022-06-04 00:12:28 -0300

    feat: finishing task

--- page3.js
+++ page3.js
@@ -1,30 +1,49 @@
 document.getElementById('btn-check').addEventListener('click', fncCheck);
 
 function fncCheck() {
+  let expectativa = 80.1 * 365.25;
+  let strGenero='feminino';  // convertendo para dias
+  const now = new Date;
+  const resultFinal= document.querySelector("#p-result");
 
-  let dia = document.querySelector("#dia").value
-  if (dia.float=0){
+  const dia = document.querySelector("#dia").value
+  if (dia==""){
     alert('Por favor, informe o dia do seu nascimento.');  
+    return false;
   }
-  let mes = document.querySelector("#mes").value
-  if (mes.float=0){
+  const mes = document.querySelector("#mes").value
+  if (mes==""){
     alert('Por favor, informe o mês do seu nascimento.');  
+    return false;
   }
-  let ano = document.querySelector("#ano").value
-  if (ano.float=0){
+  const ano = document.querySelector("#ano").value
+  if (ano==""){
     alert('Por favor, informe o ano do seu nascimento.');  
+    return false;
   }
-  let genero = document.querySelector(".ipt-gender").value;
-  
-  if (genero==null){
-    alert('Por favor, informe o seu gênero.');  
+  const genChecked = document.querySelector('input[name="genero"]:checked');
+    if (genChecked==null){
+      alert('Por favor, informe o seu gênero.');  
+      return false;
   }
-  console.log(dia);
-  if (genero==="1"){
-    let expectativa = 80.1;
-  } else {
-    let expectativa = 73.1;
+  const genero=genChecked.value;
+  if (genero=="2"){
+    expectativa = 73.1 * 365.25; // convertendo para dias
+    strGenero='masculino';  // convertendo para dias
   }  
-  
+  expectativa=expectativa.toFixed(0); //arredondando para inteiro
 
-} 
+  const dtHoje = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const dtNasc = new Date(ano, mes-1, dia);
+
+  if (dtHoje.getTime()<dtNasc.getTime()){
+    alert('Data de Nascimento maior que a data de Hoje.');  
+    return false;
+  }
+  const diff = Math.abs(dtHoje.getTime() - dtNasc.getTime()); //diff em milisegundos
+  const diasVida = Math.ceil(diff / (1000 * 60 * 60 * 24)); //convertendo milisegundos em dias
+  const diasRestantes = expectativa - diasVida; // dias de vida restantes
+  
+  resultFinal.textContent='Considerando a expectativa vida média da população brasileira para o gênero ' + strGenero + ', você tem mais ' + diasRestantes + ' dias de vida.';
+
+}
