2596aa659e80432187fbcad2846af4472cff10b0
Rogerio Kotsubo <rogeriokotsubo@gmail.com>
2022-06-04 12:38:42 -0300

    chore: add details

--- page3.js
+++ page3.js
@@ -11,26 +11,29 @@ function fncCheck() {
     alert('Por favor, informe o dia do seu nascimento.');  
     return false;
   }
+
   const mes = document.querySelector("#mes").value
   if (mes==""){
     alert('Por favor, informe o mês do seu nascimento.');  
     return false;
   }
+
   const ano = document.querySelector("#ano").value
   if (ano==""){
     alert('Por favor, informe o ano do seu nascimento.');  
     return false;
   }
+
   const genChecked = document.querySelector('input[name="genero"]:checked');
-    if (genChecked==null){
-      alert('Por favor, informe o seu gênero.');  
-      return false;
+  if (genChecked==null){
+    alert('Por favor, informe o seu gênero.');  
+    return false;
   }
   const genero=genChecked.value;
   if (genero=="2"){
     expectativa = 73.1 * 365.25; // convertendo para dias
-    strGenero='masculino';  // convertendo para dias
-  }  
+    strGenero='masculino';  
+  } 
   expectativa=expectativa.toFixed(0); //arredondando para inteiro
 
   const dtHoje = new Date(now.getFullYear(), now.getMonth(), now.getDate());
@@ -43,7 +46,9 @@ function fncCheck() {
   const diff = Math.abs(dtHoje.getTime() - dtNasc.getTime()); //diff em milisegundos
   const diasVida = Math.ceil(diff / (1000 * 60 * 60 * 24)); //convertendo milisegundos em dias
   const diasRestantes = expectativa - diasVida; // dias de vida restantes
-  
+  if (diasRestantes < 0){
+    resultFinal.textContent='Considerando a expectativa vida média da população brasileira para o gênero ' + strGenero + ', você ultrapassou sua expectativa de vida em ' + Math.abs(diasRestantes) + ' dias.';
+  } else{    
   resultFinal.textContent='Considerando a expectativa vida média da população brasileira para o gênero ' + strGenero + ', você tem mais ' + diasRestantes + ' dias de vida.';
-
-}
+  }
+ }
